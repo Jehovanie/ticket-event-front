665e46c19788e9e5344945675cbdb5e299f6d02d
Jehovanie RAM <jehovanieram@gmail.com>
2025-04-17 07:10:59 +0300

    update form create event

--- src/app/page/dashboard/home/components/create-event/create-event.component.ts
+++ src/app/page/dashboard/home/components/create-event/create-event.component.ts
@@ -30,20 +30,41 @@ export class CreateEventComponent implements OnInit {
     description: WritableSignal<string>;
     startedAt: WritableSignal<Date>;
     endAt: WritableSignal<Date>;
-    location: WritableSignal<string>;
     imageUrl: WritableSignal<string[]>;
-    category: WritableSignal<string>;
-    createdAt: WritableSignal<Date>;
-    updatedAt: WritableSignal<Date>;
-    organizer: WritableSignal<string>;
+
+    locationId: WritableSignal<number | null>;
+    locationName: WritableSignal<string | null>;
+    locationSize: WritableSignal<number | null>;
+
+    categoryId: WritableSignal<number | null>;
+    categoryName: WritableSignal<string | null>;
+    categoryColor: WritableSignal<string | null>;
+
+    organizerId: WritableSignal<number | null>;
+    organizerName: WritableSignal<string | null>;
+    organizerEmail: WritableSignal<string | null>;
+    organizerPhone: WritableSignal<string | null>;
+    organizerAddress: WritableSignal<string | null>;
+    organizerWebsite: WritableSignal<string | null>;
+
+    tickets: WritableSignal<
+      {
+        id: Signal<number>;
+        name: Signal<string>;
+        price: Signal<number>;
+        size: Signal<number>;
+      }[]
+    >;
   };
 
   isValidEvent!: {
     title: Signal<boolean>;
     description: Signal<boolean>;
-    category: Signal<boolean>;
   };
 
+  isCreateNewLocation: boolean = false;
+  isCreateNewOrganizer: boolean = false;
+  isCreateNewCategory: boolean = false;
   isSubmitting: boolean = false;
 
   categories!: ICategory[];
@@ -63,27 +84,98 @@ export class CreateEventComponent implements OnInit {
       description: signal<string>(''),
       startedAt: signal<Date>(new Date()),
       endAt: signal<Date>(new Date()),
-      location: signal<string>(''),
       imageUrl: signal<string[]>([]),
-      category: signal<string>(''),
-      createdAt: signal<Date>(new Date()),
-      updatedAt: signal<Date>(new Date()),
-      organizer: signal<string>(''),
+
+      tickets: signal<
+        {
+          id: Signal<number>;
+          name: Signal<string>;
+          price: Signal<number>;
+          size: Signal<number>;
+        }[]
+      >([]),
+
+      locationId: signal<number | null>(null),
+      locationName: signal<string | null>(null),
+      locationSize: signal<number | null>(null),
+
+      categoryId: signal<number | null>(null),
+      categoryName: signal<string | null>(null),
+      categoryColor: signal<string | null>(null),
+
+      organizerId: signal<number | null>(null),
+      organizerName: signal<string | null>(null),
+      organizerEmail: signal<string | null>(null),
+      organizerPhone: signal<string | null>(null),
+      organizerAddress: signal<string | null>(null),
+      organizerWebsite: signal<string | null>(null),
     };
 
     this.isValidEvent = {
       title: computed<boolean>(() => this.event.title().trim().length >= 3),
       description: computed(() => this.event.description().trim().length >= 3),
-      category: computed(() => this.event.category().trim().length >= 3),
     };
 
     this.initAllData();
+
+    this.addTicket();
   }
 
   initAllData() {
     this.initCategories();
     this.initOrganizers();
     this.initLocations();
+  }
+
+  updateTicketName(index: number, value: string) {
+    const updated = this.event.tickets().map((ticket) => {
+      if (ticket.id() === index) {
+        ticket.name = signal(value);
+      }
+      return ticket;
+    });
+
+    this.event.tickets.set(updated);
+  }
+
+  updateTicketPrice(index: number, value: number) {
+    const updated = this.event.tickets().map((ticket) => {
+      if (ticket.id() === index) {
+        ticket.price = signal(+value);
+      }
+      return ticket;
+    });
+
+    this.event.tickets.set(updated);
+  }
+
+  updateTicketSize(index: any, value: number) {
+    const updated = this.event.tickets().map((ticket) => {
+      if (ticket.id() === index) {
+        ticket.size = signal(+value);
+      }
+      return ticket;
+    });
+
+    this.event.tickets.set(updated);
+  }
+
+  addTicket() {
+    const radomID = Math.floor(Math.random() * 100);
+    const newTicket = {
+      id: signal<number>(radomID),
+      name: signal<string>(''),
+      price: signal<number>(0),
+      size: signal<number>(0),
+    };
+
+    this.event.tickets.set([...this.event.tickets(), newTicket]);
+  }
+
+  removeTicket(ticketID: Number) {
+    this.event.tickets.set([
+      ...this.event.tickets().filter((ticket) => ticket.id() !== ticketID),
+    ]);
   }
 
   async initCategories() {
@@ -120,11 +212,7 @@ export class CreateEventComponent implements OnInit {
   }
 
   isFormValid = computed(() => {
-    return (
-      this.isValidEvent.title() &&
-      this.isValidEvent.description() &&
-      this.isValidEvent.category()
-    );
+    return this.isValidEvent.title() && this.isValidEvent.description();
   });
 
   openModal() {
@@ -141,21 +229,58 @@ export class CreateEventComponent implements OnInit {
     }
   }
 
+  onSelectLocation(event: any) {
+    console.log(event.target.value);
+    const selectedLocation = this.locations.find(
+      (location) => +location.id === +event.target.value
+    );
+
+    if (selectedLocation) {
+      this.event.locationId.set(parseInt(selectedLocation.id));
+      this.event.locationName.set(selectedLocation.name);
+      this.event.locationSize.set(selectedLocation.size);
+    }
+  }
+
   createDataMapping(event: any): IEvent {
-    let organizer =
-      typeof event.organizer() === 'string'
-        ? `/api/organizer/${event.organizer()}`
-        : event.organizer();
+    const location = {
+      id: event.locationId(),
+      name: event.locationName(),
+      size: event.locationSize(),
+    };
 
-    let category =
-      typeof event.category() === 'string'
-        ? `/api/category/${event.category()}`
-        : event.category();
+    const category = {
+      id: event.categoryId(),
+      name: event.categoryName(),
+      color: event.categoryColor(),
+    };
 
-    let location =
-      typeof event.location() === 'string'
-        ? `/api/location/${event.location()}`
-        : event.location();
+    const organizer = {
+      id: event.organizerId(),
+      name: event.organizerName(),
+      email: event.organizerEmail(),
+      phone: event.organizerPhone(),
+      address: event.organizerAddress(),
+      website: event.organizerWebsite(),
+    };
+
+    const tickets = event
+      .tickets()
+      .map(
+        (ticket: {
+          id: Signal<number>;
+          name: Signal<string>;
+          price: Signal<number>;
+          size: Signal<number>;
+        }) => {
+          return {
+            id: ticket.id(),
+            name: ticket.name(),
+            price: ticket.price(),
+            size: ticket.size(),
+          };
+        }
+      );
 
     return {
       title: event.title(),
@@ -163,27 +288,27 @@ export class CreateEventComponent implements OnInit {
       startedAt: event.startedAt(),
       endAt: event.endAt(),
       imageUrl: event.imageUrl(),
-      createdAt: event.createdAt(),
-      updatedAt: event.updatedAt(),
+      status: 'pending',
       location: location,
       category: category,
       organizer: organizer,
-      status: 'pending',
+      tickets: tickets,
     };
   }
 
   async onSubmit() {
     if (!this.isValidEvent) {
-      alert('Please fill all the fields correctly.');
+      console.log('Please fill all the fields correctly.');
       return;
     }
     const newEvent: IEvent = this.createDataMapping(this.event);
+    console.log(newEvent);
 
     this.isSubmitting = true;
 
-    console.log(newEvent);
+    // console.log(newEvent);
 
-    await this.handleCreateEvent(newEvent);
+    // await this.handleCreateEvent(newEvent);
   }
 
   async handleCreateEvent(event: IEvent) {
@@ -205,10 +330,23 @@ export class CreateEventComponent implements OnInit {
     this.event.description.set('');
     this.event.startedAt.set(new Date());
     this.event.endAt.set(new Date());
-    this.event.location.set('');
     this.event.imageUrl.set([]);
-    this.event.category.set('');
-    this.event.createdAt.set(new Date());
-    this.event.updatedAt.set(new Date());
+
+    this.event.tickets.set([]);
+
+    this.event.locationId.set(null);
+    this.event.locationName.set('');
+    this.event.locationSize.set(null);
+
+    this.event.categoryId.set(null);
+    this.event.categoryName.set('');
+    this.event.categoryColor.set('');
+
+    this.event.organizerId.set(null);
+    this.event.organizerName.set('');
+    this.event.organizerEmail.set('');
+    this.event.organizerPhone.set('');
+    this.event.organizerAddress.set('');
+    this.event.organizerWebsite.set('');
   }
 }
